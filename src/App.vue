022db44fa6264f3f85c273a10f41123735f87f44
caihaibjb <1027747358@qq.com>
2019-03-30 21:40:03 +0800

    清空了项目模板

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>caihai</div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
